b04b5d6a80f5ec452c26b050170bbe2afeac8f68
Omkar <omkar.mahajan872@gmail.com>
2022-06-20 19:01:58 +0530

    bonus feature working

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('create_team', views.create_team, name='create_team'),
     path('dividend', views.dividend, name='dividend'),
+    path('bonus', views.bonus, name='bonus'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -36,3 +36,39 @@ def dividend(request):
 
         
     return render(request, "mainapp_dividend.html", {'stocks' : stocklist}) 
+
+
+
+def bonus(request):
+    stocks = Stock.objects.all()
+    teams = Team.objects.all()
+    stocklist = []
+    for stock in stocks:
+        stocklist.append({'stock_name' : stock.stock_name})
+
+    if request.method == 'POST':
+        stock_name = request.POST['stock_name']
+        stock = Stock.objects.get(stock_name = stock_name)
+        stock_price = stock.stock_price
+        bonus_percentage = float(request.POST['bonus_percentage'])
+        for team in teams:
+            portfolio = team.portfolio
+            team_balance = team.team_balance
+            number_of_stocks_held = float(portfolio[stock_name])
+            bonus_to_be_given = bonus_percentage * number_of_stocks_held * 0.01
+            
+            if number_of_stocks_held + bonus_to_be_given > 2000:
+                portfolio[stock_name] = str(2000)
+                excess_bonus_in_amount = (number_of_stocks_held + bonus_to_be_given - 2000) * stock_price
+                team_balance = team_balance + excess_bonus_in_amount
+                team.team_balance = team_balance
+                team.portfolio = portfolio
+            
+            else:
+                portfolio[stock_name] = str(int(bonus_to_be_given + number_of_stocks_held))
+                team.portfolio = portfolio
+
+
+            team.team_balance = team_balance
+            team.save()
+    return render(request, "mainapp_bonus.html", {'stocks' : stocklist})
